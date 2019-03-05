6044075befee37d08d0d00bf8ec5991eb523b32a
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-04 21:12:49 -0500

    Cascade drop down list

--- Shop.Web/Controllers/AccountController.cs
+++ Shop.Web/Controllers/AccountController.cs
@@ -13,18 +13,22 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.IdentityModel.Tokens;
     using Models;
+    using Shop.Web.Data;
 
     public class AccountController : Controller
     {
         private readonly IUserHelper userHelper;
         private readonly IConfiguration configuration;
+        private readonly ICountryRepository countryRepository;
 
         public AccountController(
             IUserHelper userHelper,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            ICountryRepository countryRepository)
         {
             this.userHelper = userHelper;
             this.configuration = configuration;
+            this.countryRepository = countryRepository;
         }
 
         public IActionResult Login()
@@ -66,7 +70,13 @@
 
         public IActionResult Register()
         {
-            return this.View();
+            var model = new RegisterNewUserViewModel
+            {
+                Countries = this.countryRepository.GetComboCountries(),
+                Cities = this.countryRepository.GetComboCities(0)
+            };
+
+            return this.View(model);
         }
 
         [HttpPost]
@@ -77,12 +87,18 @@
                 var user = await this.userHelper.GetUserByEmailAsync(model.Username);
                 if (user == null)
                 {
+                    var city = await this.countryRepository.GetCityAsync(model.CityId);
+
                     user = new User
                     {
                         FirstName = model.FirstName,
                         LastName = model.LastName,
                         Email = model.Username,
-                        UserName = model.Username
+                        UserName = model.Username,
+                        Address = model.Address,
+                        PhoneNumber = model.PhoneNumber,
+                        CityId = model.CityId,
+                        City = city
                     };
 
                     var result = await this.userHelper.AddUserAsync(user, model.Password);
@@ -125,7 +141,25 @@
             {
                 model.FirstName = user.FirstName;
                 model.LastName = user.LastName;
+                model.Address = user.Address;
+                model.PhoneNumber = user.PhoneNumber;
+
+                var city = await this.countryRepository.GetCityAsync(user.CityId);
+                if (city != null)
+                {
+                    var country = await this.countryRepository.GetCountryAsync(city);
+                    if (country != null)
+                    {
+                        model.CountryId = country.Id;
+                        model.Cities = this.countryRepository.GetComboCities(country.Id);
+                        model.Countries = this.countryRepository.GetComboCountries();
+                        model.CityId = user.CityId;
+                    }
+                }
             }
+
+            model.Cities = this.countryRepository.GetComboCities(model.CountryId);
+            model.Countries = this.countryRepository.GetComboCountries();
 
             return this.View(model);
         }
@@ -138,8 +172,15 @@
                 var user = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 if (user != null)
                 {
+                    var city = await this.countryRepository.GetCityAsync(model.CityId);
+
                     user.FirstName = model.FirstName;
                     user.LastName = model.LastName;
+                    user.Address = model.Address;
+                    user.PhoneNumber = model.PhoneNumber;
+                    user.CityId = model.CityId;
+                    user.City = city;
+
                     var respose = await this.userHelper.UpdateUserAsync(user);
                     if (respose.Succeeded)
                     {
@@ -236,6 +277,12 @@
         public IActionResult NotAuthorized()
         {
             return this.View();
+        }
+
+        public async Task<JsonResult> GetCities(int countryId)
+        {
+            var country = await this.countryRepository.GetCountryAsync(countryId);
+            return this.Json(country.Cities.OrderBy(c => c.Name));
         }
     }
 }
--- Shop.Web/Data/Entities/User.cs
+++ Shop.Web/Data/Entities/User.cs
@@ -1,11 +1,19 @@
 ﻿namespace Shop.Web.Data.Entities
 {
     using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
 
     public class User : IdentityUser
     {
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        public string Address { get; set; }
+
+        public int CityId { get; set; }
+
+        public City City { get; set; }
     }
 }
