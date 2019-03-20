e470def8a98beb2cd5e40f97757a4ed3e6c73324
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-20 09:45:21 -0500

    Test in iOS Classsic

---
+++ Shop.UIClassic.iOS/ProductsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Shop.UIClassic.iOS
+{
+    public partial class ProductsViewController : UIViewController
+    {
+        public ProductsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
--- Shop.UIClassic.iOS/ViewController.cs
+++ Shop.UIClassic.iOS/ViewController.cs
@@ -1,12 +1,20 @@
 ﻿namespace Shop.UIClassic.iOS
 {
+    using Newtonsoft.Json;
+    using Shop.Common.Helpers;
+    using Shop.Common.Models;
+    using Shop.Common.Services;
     using System;
+    using System.Collections.Generic;
     using UIKit;
 
     public partial class ViewController : UIViewController
     {
+        private ApiService apiService;
+
         public ViewController(IntPtr handle) : base(handle)
         {
+            this.apiService = new ApiService();
         }
 
         public override void ViewDidLoad()
@@ -41,9 +49,61 @@
                 return;
             }
 
-            var ok = UIAlertController.Create("Ok", "Fuck yeah!", UIAlertControllerStyle.Alert);
-            ok.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
-            this.PresentViewController(ok, true, null);
+            this.DoLogin();
+        }
+
+        private async void DoLogin()
+        {
+            this.ActivityIndicator.StartAnimating();
+            var request = new TokenRequest
+            {
+                Username = this.EmailText.Text,
+                Password = this.PasswordText.Text
+            };
+
+            var response = await this.apiService.GetTokenAsync(
+                "https://shopzulu.azurewebsites.net",
+                "/Account",
+                "/CreateToken",
+                request);
+
+            if (!response.IsSuccess)
+            {
+                this.ActivityIndicator.StopAnimating();
+                var alert = UIAlertController.Create("Error", "User or password incorrect.", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+                this.PresentViewController(alert, true, null);
+                return;
+            }
+
+            var token = (TokenResponse)response.Result;
+
+            var response2 = await this.apiService.GetListAsync<Product>(
+                "https://shopzulu.azurewebsites.net",
+                "/api",
+                "/Products",
+                "bearer",
+                token.Token);
+
+            if (!response2.IsSuccess)
+            {
+                var alert = UIAlertController.Create("Error", response.Message, UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+                this.PresentViewController(alert, true, null);
+                return;
+            }
+
+            var products = (List<Product>)response2.Result;
+
+            Settings.UserEmail = this.EmailText.Text;
+            Settings.Token = JsonConvert.SerializeObject(token);
+            Settings.Products = JsonConvert.SerializeObject(products);
+            this.ActivityIndicator.StopAnimating();
+
+            var board = UIStoryboard.FromName("Main", null);
+            var productsViewController = board.InstantiateViewController("ProductsViewController");
+            productsViewController.Title = "Products";
+            this.NavigationController.PushViewController(productsViewController, true);
         }
     }
 }
