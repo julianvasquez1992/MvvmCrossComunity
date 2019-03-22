17961cad48f6ff9d94e55e3a60926b1eac7b6045
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-22 14:15:58 -0500

    Android Cross Part one

---
+++ UICross.Android/Services/DialogService.cs
@@ -0,0 +1,22 @@
+﻿namespace UICross.Android.Services
+{
+    using global::Android.App;
+    using MvvmCross;
+    using MvvmCross.Platforms.Android;
+    using Shop.Common.Interfaces;
+
+    public class DialogService : IDialogService
+    {
+        public void Alert(string title, string message, string okbtnText)
+        {
+            var top = Mvx.Resolve<IMvxAndroidCurrentTopActivity>();
+            var act = top.Activity;
+
+            var adb = new AlertDialog.Builder(act);
+            adb.SetTitle(title);
+            adb.SetMessage(message);
+            adb.SetPositiveButton(okbtnText, (sender, args) => { /* some logic */ });
+            adb.Create().Show();
+        }
+    }
+}
---
+++ UICross.Android/Views/LoginView.cs
@@ -0,0 +1,17 @@
+﻿namespace UICross.Android.Views
+{
+    using global::Android.App;
+    using global::Android.OS;
+    using MvvmCross.Platforms.Android.Views;
+    using Shop.Common.ViewModels;
+
+    [Activity(Label = "@string/app_name")]
+    public class LoginView : MvxActivity<LoginViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.LoginPage);
+        }
+    }
+}
