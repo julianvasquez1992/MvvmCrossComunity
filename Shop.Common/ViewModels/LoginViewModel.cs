9fd9be44f2d06b8d14812aa5cd9c591154fe7b6b
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-22 16:31:03 -0500

    Android Cross Second Part

--- Shop.Common/ViewModels/LoginViewModel.cs
+++ Shop.Common/ViewModels/LoginViewModel.cs
@@ -4,8 +4,11 @@
     using Interfaces;
     using Models;
     using MvvmCross.Commands;
+    using MvvmCross.Navigation;
     using MvvmCross.ViewModels;
+    using Newtonsoft.Json;
     using Services;
+    using Shop.Common.Helpers;
 
     public class LoginViewModel : MvxViewModel
     {
@@ -14,6 +17,7 @@
         private MvxCommand loginCommand;
         private readonly IApiService apiService;
         private readonly IDialogService dialogService;
+        private readonly IMvxNavigationService navigationService;
         private bool isLoading;
 
         public bool IsLoading
@@ -45,13 +49,15 @@
 
         public LoginViewModel(
             IApiService apiService,
-            IDialogService dialogService)
+            IDialogService dialogService,
+            IMvxNavigationService navigationService)
         {
             this.apiService = apiService;
             this.dialogService = dialogService;
+            this.navigationService = navigationService;
 
             this.Email = "jzuluaga55@gmail.com";
-            this.Password = "123456";
+            this.Password = "Roger1974";
             this.IsLoading = false;
         }
 
@@ -90,8 +96,12 @@
                 return;
             }
 
+            var token = (TokenResponse)response.Result;
+            Settings.UserEmail = this.Email;
+            Settings.Token = JsonConvert.SerializeObject(token);
             this.IsLoading = false;
-            this.dialogService.Alert("Ok", "Fuck yeah!", "Accept");
+
+            await this.navigationService.Navigate<ProductsViewModel>();
         }
     }
 }
---
+++ UICross.Android/Views/ProductView.cs
@@ -0,0 +1,17 @@
+﻿namespace UICross.Android.Views
+{
+    using global::Android.App;
+    using global::Android.OS;
+    using MvvmCross.Platforms.Android.Views;
+    using Shop.Common.ViewModels;
+
+    [Activity(Label = "@string/app_name")]
+    public class KittensView : MvxActivity<ProductsViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            this.SetContentView(Resource.Layout.ProductsPage);
+        }
+    }
+}
