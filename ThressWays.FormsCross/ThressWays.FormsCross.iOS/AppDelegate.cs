55be323be37d4a614e1cfd2f839bcb5bba2a5ce2
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-22 09:45:41 -0500

    MVVM Cross in Forms

---
+++ ThressWays.FormsCross/ThressWays.FormsCross.iOS/AppDelegate.cs
@@ -0,0 +1,16 @@
+﻿namespace ThressWays.FormsCross.iOS
+{
+    using Foundation;
+    using MvvmCross.Forms.Platforms.Ios.Core;
+    using ThreeWays.Core;
+    using UIKit;
+
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp>
+    {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
---
+++ ThressWays.FormsCross/ThressWays.FormsCross/Views/TipView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ThressWays.FormsCross.Views
+{
+    using MvvmCross.Forms.Views;
+    using ThreeWays.Core.ViewModels;
+
+    public partial class TipView : MvxContentPage<TipViewModel>
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
