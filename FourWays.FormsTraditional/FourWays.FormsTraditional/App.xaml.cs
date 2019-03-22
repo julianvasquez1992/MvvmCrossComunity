65c01b0d098e6173b83a3f18c38966d8d9201da0
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-03-22 13:49:50 -0500

    Four ways

---
+++ FourWays.FormsTraditional/FourWays.FormsTraditional/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FourWays.FormsTraditional
+{
+    using Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new TipPage());
+        }
+    }
+}
