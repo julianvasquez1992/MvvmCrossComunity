0b0f3f339a345f9cc8e3e9042852dddee0d6cd27
Cristian Martinez Tabares <cristian.martinez@globant.com>
2019-04-17 13:24:33 -0500

    Burger Menu

---
+++ UICross.Android/Views/AboutFrag.cs
@@ -0,0 +1,14 @@
+﻿using Android.OS;
+using Android.Views;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace UICross.Android.Views
+{
+    public class AboutFrag : Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(Resource.Layout.About, container, false);
+        }
+    }
+}
---
+++ UICross.Android/Views/SettingsFrag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.OS;
+using Android.Views;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace UICross.Android.Views
+{
+    public class SettingsFrag : Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(Resource.Layout.Settings, container, false);
+        }
+    }
+}
